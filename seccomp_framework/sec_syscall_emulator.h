a40f5d140365b33e07ce17ed5a7cebebd26415ce
schwerem <schwerem@students.zhaw.ch>
2018-04-17 15:49:58 +0200

    Readded demo files

--- seccomp_framework/sec_syscall_emulator.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEC_SYSCALL_EMULATOR_H
-
-#define SEC_SYSCALL_EMULATOR_H
-
-#include <unistd.h>
-
-void performSystemcall(pid_t pid, int status, int syscall_n, int use_after_check);
-
-#endif //SEC_SYSCALL_EMULATOR_H
